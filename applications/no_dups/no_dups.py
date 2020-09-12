f29520bbea07003162ca2b4670270779e9ecad69
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-09-11 22:10:19 -0700

    no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,23 @@
 def no_dups(s):
-    # Your code here
+    cache = []
+    split_words = s.split(" ")
+    
+    string_seperator = " "
+   
+    cache.append(split_words[0])
+    
+    
+    for word in range(1,len(split_words)):
+        if split_words[word] not in cache:
+            cache.append(split_words[word])
+        
+    return string_seperator.join(cache)
+
+    
+    
+
+                
+                
 
 
 
